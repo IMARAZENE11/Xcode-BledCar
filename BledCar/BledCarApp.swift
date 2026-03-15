cb0e94dce3072e9886325028459072a710a80881
Amine <imarazeneamine95@gmail.com>
2026-03-15 20:51:37 +0000

    Initial Commit

---
+++ BledCar/BledCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BledCarApp.swift
+//  BledCar
+//
+//  Created by user294323 on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BledCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BledCarTests/BledCarTests.swift
@@ -0,0 +1,16 @@
+//
+//  BledCarTests.swift
+//  BledCarTests
+//
+//  Created by user294323 on 3/15/26.
+//
+
+import Testing
+
+struct BledCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
